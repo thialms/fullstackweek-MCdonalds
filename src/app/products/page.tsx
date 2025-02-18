7bd16e1988218b325089e132812e06aca05d2f22
thialms <thiiago.allmeida44@gmail.com>
2025-02-18 16:39:33 -0300

    feat: shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+        <>
+            <h1>Product Page</h1>
+            <Button>FSW</Button>
+        </>
+    );
+};
+
+export default ProductPage;
